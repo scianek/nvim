bfe4762fe3a71dd6d3123f8428136da963109d6c
Mateusz Ścianek <scianek@protonmail.com>
2025-12-30 14:55:59 +0100

    feat: setup editor options

---
+++ lua/core/options.lua
@@ -0,0 +1,19 @@
+local opt = vim.opt
+
+vim.g.mapleader = " "
+
+opt.number = true
+opt.relativenumber = true
+
+opt.autoindent = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.smarttab = true
+opt.expandtab = true
+
+opt.scrolloff = 9999
+opt.termguicolors = true
+opt.laststatus = 3
+opt.cmdheight = 0
+opt.winborder = "rounded"
+opt.autoread = true
